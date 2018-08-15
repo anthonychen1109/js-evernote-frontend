47864f5310307df28a8bbc2e3da01aefe06c7c1f
github username <github email address>
2018-08-15 11:57:07 -0400

    sorry master branch

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   displayNotes()
 
   // Edit Function
-  // function editPost(id) {
-  //   const notesURL = `http://localhost:3000/api/v1/notes${id}`
-  //   fetch()
-  // }
+  function editPost(id) {
+    const notesURL = `http://localhost:3000/api/v1/notes${id}`
+    fetch()
+  }
 
   // fetch notes
   function fetchNotes() {
@@ -26,7 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function grabNotes() {
     return fetchUsers().then(notes => notes.notes)
+  }
 
+  function fetchNote(id) {
+    return fetch(`http://localhost:3000/api/v1/notes/`)
+    .then(response => response.json())
+    .then(data => data[id])
   }
 
   function showBody(e){
@@ -43,25 +48,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
           // CRUD Buttons
           const btnDiv = document.getElementById('btns-btns')
+          const createBtnDiv = document.createElement('div')
+          const editBtnDiv = document.createElement('div')
+          const deleteBtnDiv = document.createElement('div')
           btnDiv.innerHTML = ''
           //Edit
           const editBtn = document.createElement('button')
           editBtn.innerText = 'Edit'
+          editBtn.classList.add('crud-btn')
           editBtn.dataset.id = note.id
+          editBtn.id = `edit-${note.id}`
           editBtn.addEventListener('click', editRender)
           //Create
           const createBtn = document.createElement('button')
           createBtn.innerText = 'Create'
+          createBtn.classList.add('crud-btn')
           createBtn.dataset.id = note.id
           // createBtn.addEventListener('click', createPost)
           //Delete
           const deleteBtn = document.createElement('button')
           deleteBtn.innerText = 'Delete'
+          deleteBtn.classList.add('crud-btn')
           deleteBtn.dataset.id = note.id
           // deleteBtn.addEventListener('click', deletePost)
 
-
-          btnDiv.append(createBtn, deleteBtn, editBtn)
+          createBtnDiv.append(createBtn)
+          editBtnDiv.append(editBtn)
+          deleteBtnDiv.append(deleteBtn)
+          btnDiv.append(createBtnDiv, deleteBtnDiv, editBtnDiv)
           div.append(h, p)
         }}
       ))
@@ -71,8 +85,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const id = e.target.dataset.id
     const div = document.getElementById('preview-window')
     div.innerText = ''
-
-
+    const form = document.createElement('form')
+    // title
+    // body
+    // submitbutton
+    fetchNote(id)
+      .then(note => {
+        // h.innerText = note.title
+        div.innerText = ''
+        // p.innerText = note.body
+        const titleField = document.createElement('input')
+        titleField.setAttribute('type', 'text')
+        titleField.setAttribute('value', note.title)
+        const bodyField = document.createElement('input')
+        bodyField.setAttribute('type', 'text')
+        bodyField.setAttribute('value', note.body)
+        const submitBtn = document.getElementById(`edit-${id}`)
+        submitBtn.innerText = "Submit"
+        submitBtn.addEventListener('click', editPost)
+        form.appendChild(titleField)
+        form.appendChild(bodyField)
+        div.append(form)
+      })
   }
 
   function displayNotes(){
