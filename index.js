2c630dc46ee9ed01101581bf01b0fa68b993244c
runandrerun <the.asantiagojr@gmail.com>
2018-08-15 11:27:36 -0400

    all the changes bro

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // displayNotes()
   displayNotes()
 
+  // Edit Function
+  // function editPost(id) {
+  //   const notesURL = `http://localhost:3000/api/v1/notes${id}`
+  //   fetch()
+  // }
+
   // fetch notes
   function fetchNotes() {
     fetch("http://localhost:3000/api/v1/notes")
@@ -29,12 +35,44 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(notes => notes.forEach(note => {
         if(note.id == toShow){
           const div = document.getElementById('preview-window')
+          const h = document.createElement('h2')
+          h.innerText = note.title
           const p = document.createElement('p')
           div.innerText = ''
           p.innerText = note.body
-          div.append(p)
+
+          // CRUD Buttons
+          const btnDiv = document.getElementById('btns-btns')
+          btnDiv.innerHTML = ''
+          //Edit
+          const editBtn = document.createElement('button')
+          editBtn.innerText = 'Edit'
+          editBtn.dataset.id = note.id
+          editBtn.addEventListener('click', editRender)
+          //Create
+          const createBtn = document.createElement('button')
+          createBtn.innerText = 'Create'
+          createBtn.dataset.id = note.id
+          // createBtn.addEventListener('click', createPost)
+          //Delete
+          const deleteBtn = document.createElement('button')
+          deleteBtn.innerText = 'Delete'
+          deleteBtn.dataset.id = note.id
+          // deleteBtn.addEventListener('click', deletePost)
+
+
+          btnDiv.append(createBtn, deleteBtn, editBtn)
+          div.append(h, p)
         }}
       ))
+  }
+
+  function editRender(e) {
+    const id = e.target.dataset.id
+    const div = document.getElementById('preview-window')
+    div.innerText = ''
+
+
   }
 
   function displayNotes(){
